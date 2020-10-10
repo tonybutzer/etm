c0591b0c29e9e7217f33c314734f4b9e40c78f7d
tonybutzer <tonybutzer@gmail.com>
2020-10-10 23:41:37 +0000

    automatic git update from Makefile

--- Dockerfile
+++ Dockerfile
@@ -27,7 +27,7 @@ RUN \
 	
 
 
-ENV TONY_VERS=1.4
+ENV TONY_VERS=1.5
 RUN mkdir -p /home/etm \
 	&& mkdir -p /home/etm/api_etm/log
 
---
+++ shell/drb150/cmd_run_favorite.sh
@@ -0,0 +1,5 @@
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y all etasw netet &
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y all dd netet &
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y all srf netet &
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y all netet netet &
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y all rain netet &
