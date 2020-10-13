f51f3b0ddbc0a40c6a2b78cf2265fce240acdcdb
tonybutzer <tonybutzer@gmail.com>
2020-10-13 03:30:46 +0000

    automatic git update from Makefile

---
+++ shell/drb150/select_product_cmd_runner_netet.sh
@@ -0,0 +1,17 @@
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1961_1965 netet dummy  2>&1 | tee  ./log/etm1years_1961_1965&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1966_1969 netet dummy  2>&1 | tee  ./log/etm1years_1966_1969&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1973_1978 netet dummy  2>&1 | tee  ./log/etm1years_1973_1978&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1979_1984 netet dummy  2>&1 | tee  ./log/etm1years_1979_1984&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1985_1989 netet dummy  2>&1 | tee  ./log/etm1years_1985_1989&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1993_1998 netet dummy  2>&1 | tee  ./log/etm1years_1993_1998&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_1999_1999 netet dummy  2>&1 | tee  ./log/etm1years_1999_1999&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2002_2007 netet dummy  2>&1 | tee  ./log/etm1years_2002_2007&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2008_2009 netet dummy  2>&1 | tee  ./log/etm1years_2008_2009&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2013_2018 netet dummy  2>&1 | tee  ./log/etm1years_2013_2018&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2019_2019 netet dummy  2>&1 | tee  ./log/etm1years_2019_2019&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2022_2027 netet dummy  2>&1 | tee  ./log/etm1years_2022_2027&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2028_2033 netet dummy  2>&1 | tee  ./log/etm1years_2028_2033&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2034_2039 netet dummy  2>&1 | tee  ./log/etm1years_2034_2039&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2041_2046 netet dummy  2>&1 | tee  ./log/etm1years_2041_2046&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2047_2049 netet dummy  2>&1 | tee  ./log/etm1years_2047_2049&
+docker run -i tbutzer/etm_docker_image python3 api_etm.py -i out/DelawareRiverBasin/Run10_07_2020/ -o enduser/DelawareRiverBasin/drb150/ -y years_2053_2058 netet dummy  2>&1 | tee  ./log/etm1years_2053_2058&
