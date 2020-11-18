d32d19ec4c06aea2c26a040d40743525463c3c3c
tonybutzer <tonybutzer@gmail.com>
2020-11-18 11:57:48 +0000

    automatic git update from Makefile

--- shell/drb150/check_counts.sh
+++ shell/drb150/check_counts.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-product=netet
+product=dd
 
 
 #for i in {1950..1980} ; do echo $i; aws s3 ls dev-et-data/enduser/DelawareRiverBasin/drb150/$i/ | grep $product | wc ; done
 #for i in `seq 1950 10 2100`; do echo $i; done
 #for i in  `seq 1950 10 2100`; do echo $i; aws s3 ls dev-et-data/enduser/DelawareRiverBasin/drb150/$i/ | grep $product | wc ; done
-for i in {1950..2099} ; do echo $i; aws s3 ls dev-et-data/enduser/DelawareRiverBasin/drb150/$i/ | grep $product | wc ; done
+for i in {1950..2099} ; do echo $i; aws s3 ls dev-et-data/enduser/DelawareRiverBasin/drb150_nofunswitch/$i/ | grep $product | wc ; done
+#dev-et-data/enduser/DelawareRiverBasin/drb150_nofunswitch/
